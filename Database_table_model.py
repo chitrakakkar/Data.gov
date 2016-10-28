1c3308d85518e093c986bd00fed0af9c2c293e67
chitrakakkar <chitrakakkar@yahoo.co.in>
2016-10-28 00:39:27 -0500

    Documentation fixed

--- Database_table_model.py
+++ Database_table_model.py
@@ -1,12 +1,6 @@
-
-
 '''This is where the peewee model for a book will be stored'''
+
 from peewee import *
-
-"""Make sure that this object stores the goodreads _id.  We will need it later.
-    https://www.goodreads.com/search/index.xml?q=Ender&key=4ylN8OWi1dhG5Yhq3PQstA
-    that will show you what types of data the server responds with."""
-
 db = SqliteDatabase("CK.db")
 
 

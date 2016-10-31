1ab184c31e71a9df0ff3cbb7f7ebda8a057b09e5
chitrakakkar <chitrakakkar@yahoo.co.in>
2016-10-31 14:46:31 -0500

    GUI Documneted

--- GUI.py
+++ GUI.py
@@ -8,12 +8,13 @@ import wx.grid
 class Data_Gov_Gui(wx.Frame):
     def __init__(self, parent):
         wx.Frame.__init__(self, parent, size=(1100, 700))  # Bring everything from __init__ for Frame into this class
-        # GUI APPEARANCE (LAYOUT AND DESIGN)
+        # --------GUI APPEARANCE (LAYOUT AND DESIGN)---------
         self.panel = wx.Panel(self, size=(500, 500))
         # self.SetBackgroundColour('pink')
         self.SetBackgroundColour((198, 235, 242))
         self.SetTitle('Data Gov JOBS')
         self.CreateStatusBar()
+        # --------MEnu Designed----------------
         self.menu = wx.Menu()
         self.menu.Append(wx.ID_ABOUT, "About", "wxPython GUI")
         self.menu.AppendSeparator()
@@ -27,48 +28,62 @@ class Data_Gov_Gui(wx.Frame):
         self.menuBar.Bind(wx.EVT_MENU, self.OnQuitButton, self.menuBar)
         self.Show(True)
         bold_font = wx.Font(15, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
+        # ----------------Database Mode check-Box---------------
         self.Database_Select = wx.CheckBox(self.panel, 10, 'Database_Mode', pos=(600, 10))
         self.Database_Select.SetValue(False)
+        # ------------Search text Box -------------------------
         self.search_Txt = wx.TextCtrl(self.panel, pos=(400, 100), size=(400, 25), style=wx.ALIGN_LEFT, value='Enter the keyword')
+        # ----------Search button------------------
         self.Search_button = wx.Button(self.panel, pos=(800, 100), size=(30, 25), label="S")
         self.Search_button.SetForegroundColour('orange')
+        # ---------------All jobs check box---------------
         self.All_Jobs = wx.CheckBox(self.panel, 10, 'All Jobs', pos=(50, 200))
         self.All_Jobs.SetValue(False)
+        # ----------------Locations based check box ----------------------
         self.Location_Based = wx.CheckBox(self.panel, 10, 'Location-Based', pos=(250, 200))
         self.Location_Based.SetValue(False)
+        # ----------------Part-Time check box ----------------------
         self.Part_Time = wx.CheckBox(self.panel, 10, 'Part-Time', pos=(500, 200))
         self.Part_Time .SetValue(False)
+        # ----------------Full-Time check box ----------------------
         self.Full_Time = wx.CheckBox(self.panel, 10, 'Full-Time', pos=(750, 200))
         self.Full_Time.SetValue(False)
+        # ----------------Specific-jobs check box ----------------------
         self.Specific_Job = wx.CheckBox(self.panel, 10, 'Specific-Jobs', pos=(975, 200))
         self.Specific_Job.SetValue(False)
+        # -----------------Grid Display ----------------
         self.display_Txt = wx.grid.Grid(self.panel, pos=(10, 300), id=1, name="Search-result", size=(1080, 170))
         color = (255, 255, 255)
         color2 = 206, 133, 226
         self.display_Txt.SetDefaultCellBackgroundColour(color)
         self.display_Txt.SetForegroundColour(color2)
+        # ---------------Search-button -----------------
         self.Search_button.SetBackgroundColour((141, 221, 247))
         self.Search_button.SetFont(bold_font)
         self.Search_button.Bind(wx.EVT_BUTTON, self.OnSearchcButton)
+        # ---------------Quit-button -----------------
         self.Quit_Button = wx.Button(self.panel, pos=(30, 600), size=(100, -1), label="Quit")
         self.Quit_Button.Bind(wx.EVT_BUTTON, self.OnQuitButton)
         self.Quit_Button.SetBackgroundColour((206, 133, 226))  # orange
         self.Quit_Button.SetFont(bold_font)
+        # ---------------Clear-button -----------------
         self.Clear_Button = wx.Button(self.panel, pos=(300, 600), size=(100, -1), label="Clear")
         self.Clear_Button.Bind(wx.EVT_BUTTON, self.OnClearButton)
         self.Clear_Button.SetBackgroundColour((206, 133, 226))  # orange
         self.Clear_Button.SetFont(bold_font)
+        # ---------------Refresh-button -----------------
         self.Refresh_Button = wx.Button(self.panel, pos=(600, 600), size=(100, -1), label="Refresh")
         self.Refresh_Button.Bind(wx.EVT_BUTTON, self.OnRefreshButton)
         self.Refresh_Button.SetBackgroundColour((206, 133, 226))  # orange
         self.Refresh_Button.SetFont(bold_font)
         self.Refresh_Button.Disable()
+        # ---------------Save-button -----------------
         self.Save_Button = wx.Button(self.panel, pos=(900, 600), size=(100, -1), label="Save")
         self.Save_Button.Bind(wx.EVT_BUTTON, self.OnSaveButton)
         self.Save_Button.SetBackgroundColour((206, 133, 226))  # orange
         self.Save_Button.SetFont(bold_font)
 
-    # @staticmethod
+    # This calls the API to fetch all the jobs data.
     def OnSearchcButton(self, e):
         # displays the jobs which is a list of dictionaries
         jobs = ap.all_job()
